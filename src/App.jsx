b6eb22f14584f9c1a57b574a5cc8d943cab92114
sreeramss <sreeramnov3@gmail.com>
2024-09-10 12:02:13 +0530

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+       <ToastContainer />
+    </>
+  )
+}
+
+export default App
